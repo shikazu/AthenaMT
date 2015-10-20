192d694bd1f3b19cf0a8606c2fc6a94511869be6
shikazu <steven.schwarz90@yahoo.de>
2015-10-20 08:14:59 +0200

    Improved Main, Failed with knowledge on io_service and threads :P

--- AccountMT/main.cpp
+++ AccountMT/main.cpp
@@ -6,33 +6,18 @@
 /*#include "../Common/3rdParty/chaiscript/chaiscript.hpp"
 #include "../Common/3rdParty/chaiscript/chaiscript_stdlib.hpp"*/
 
-#include <thread>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
 
-boost::asio::io_service io_service_account;
-boost::asio::io_service io_service_connector;
-
-void run_connector_server()
-{
-	io_service_connector.run();
-}
-
-void run_account_server()
-{
-	io_service_account.run();
-}
+boost::asio::io_service io_service;
 
 int main(int argc, char** argv)
 {
-	Connector::ConnectorServer connector_server(io_service_connector, 25000);
-	Account::AccountServer account_server(io_service_account, 6900);
+	Connector::ConnectorServer connector_server(io_service, 25000);
+	Account::AccountServer account_server(io_service, 6900);
 
-	std::thread *t_account = new std::thread(run_account_server);
-	std::thread *t_connector = new std::thread(run_connector_server);
-
-	while (true)
-	{
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
+	std::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
+	t.join();
 
 	return 0;
 }
